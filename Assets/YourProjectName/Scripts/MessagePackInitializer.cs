f16d9776da010da18712e3da585e262757b61262
SAM-tak <osamu.takasugi@gmail.com>
2021-12-26 19:17:09 +0900

    apply lz4 compression to messagepack settings

--- Assets/YourProjectName/Scripts/MessagePackInitializer.cs
+++ Assets/YourProjectName/Scripts/MessagePackInitializer.cs
@@ -8,6 +8,6 @@ public static class MessagePackInitializer
     public static void Initialize()
     {
         StaticCompositeResolver.Instance.Register(GeneratedResolver.Instance, StandardResolver.Instance);
-        MessagePackSerializer.DefaultOptions = MessagePackSerializerOptions.Standard.WithResolver(StaticCompositeResolver.Instance);
+        MessagePackSerializer.DefaultOptions = MessagePackSerializerOptions.Standard.WithCompression(MessagePackCompression.Lz4BlockArray).WithResolver(StaticCompositeResolver.Instance);
     }
 }
