d9814ad76a1bca0be597012a7c2a2ea34b182df9
SAM-tak <osamu.takasugi@gmail.com>
2022-01-26 18:09:32 +0900

    stripped and add

--- Assets/YourGameClient/Scripts/Editor/BuildIos.cs
+++ Assets/YourGameClient/Scripts/Editor/BuildIos.cs
@@ -1,4 +1,4 @@
-#if UNITY_IPHONE
+#if UNITY_EDITOR && UNITY_IOS
 using System.IO;
 using UnityEngine;
 using UnityEditor;
@@ -18,12 +18,17 @@ public class BuildIos
         var projectPath = PBXProject.GetPBXProjectPath(path);
         var project = new PBXProject();
         project.ReadFromString(File.ReadAllText(projectPath));
+#if UNITY_2019_3_OR_NEWER
+        var targetGuid = project.GetUnityFrameworkTargetGuid();
+#else
         var targetGuid = project.TargetGuidByName(PBXProject.GetUnityTargetName());
+#endif
 
         // libz.tbd for grpc ios build
         project.AddFrameworkToProject(targetGuid, "libz.tbd", false);
 
         // libgrpc_csharp_ext missing bitcode. as BITCODE exand binary size to 250MB.
+        // bitode is disabled for libgrpc_csharp_ext, so need to disable it for the whole project
         project.SetBuildProperty(targetGuid, "ENABLE_BITCODE", "NO");
         
         File.WriteAllText(projectPath, project.WriteToString());
